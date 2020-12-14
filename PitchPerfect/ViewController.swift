9286584fb01e6f5460a8cd3421bed08a21e93d8d
Renan <renan.baialuna@gmail.com>
2020-12-14 18:44:40 -0300

    Add seccond screen

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called")
+        stopRecordButton.isEnabled = false
+    }
 
-
+    @IBAction func recordAudio(_ sender: AnyObject) {
+        recordingLabel.text = "recording in progress"
+        recordButton.isEnabled = false
+        stopRecordButton.isEnabled = true
+    }
+    @IBAction func stopRecording(_ sender: Any) {
+         recordingLabel.text = "tap to record"
+        recordButton.isEnabled = true
+        stopRecordButton.isEnabled = false
+    }
 }
 
